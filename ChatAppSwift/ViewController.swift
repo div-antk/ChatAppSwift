73c56c100321b34891e85178b18ab72d45efbf2f
div-antk <51283468+owaii@users.noreply.github.com>
2021-01-27 19:50:30 +0900

    Initial Commit

---
+++ ChatAppSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatAppSwift
+//
+//  Created by Takuya Ando on 2021/01/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
