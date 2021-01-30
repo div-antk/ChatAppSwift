0a5560e53aaf4596fe674d71c5004e408dd626f5
div-antk <51283468+owaii@users.noreply.github.com>
2021-01-30 20:15:28 +0900

    カスタムセルを角丸

--- ChatAppSwift/MessageCell.swift
+++ ChatAppSwift/MessageCell.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class MessageCell: UITableViewCell {
 
+    @IBOutlet weak var leftImageView: UIImageView!
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var backView: UIView!
+    @IBOutlet weak var label: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+
+        leftImageView.layer.cornerRadius = 25.0
+        rightImageView.layer.cornerRadius = 25.0
+        backView.layer.cornerRadius = 10
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
